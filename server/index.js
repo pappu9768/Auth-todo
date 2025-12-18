8574945ba00d779915761165661d78c924d42377
Pawan <9930pawanshah@gmail.com>
2025-12-18 23:24:20 +0530

    deploy

--- server/index.js
+++ server/index.js
@@ -5,9 +5,29 @@ import cors from 'cors'
 import router from './routes/authRoutes.js';
 import todoRoutes from './routes/todoRoutes.js';
 import { todoMiddleware } from './middlewares/todoMiddleware.js';
-import authModel from './models/authModel.js';
+
 dotenv.config()
 const app = express();
+
+let isConnected = false;
+async function connectDB() {
+    try {
+        const url = process.env.MONGO_URL;
+        await mongoose.connect(url)
+        isConnected = true;
+        console.log("Database connected successfully")
+
+    } catch (error) {
+        console.log("Error found while connection with database:",error)
+    }
+}
+
+app.use((req,res,next) => {
+    if(!isConnected){
+        connectDB()
+    }
+    next()
+})
 
 //imp middlewares
 app.use(express.json());
@@ -35,19 +55,14 @@ app.get('/api/me',todoMiddleware, async(req,res) => {
 
 
 //database connection
-const connectDB = async () => {
-    try {
-        const url = process.env.MONGO_URL;
-        await mongoose.connect(url)
-        console.log("Database connected successfully")
 
-    } catch (error) {
-        console.log("Error found while connection with database:",error)
-    }
-}
-const port = process.env.PORT || 5000
 
-app.listen(port, () => {
-    connectDB();
-    console.log(`server is running in ${port}`);
-})
+
+// const port = process.env.PORT || 5000
+
+// app.listen(port, () => {
+//     connectDB();
+//     console.log(`server is running in ${port}`);
+// })
+
+module.exports = app
